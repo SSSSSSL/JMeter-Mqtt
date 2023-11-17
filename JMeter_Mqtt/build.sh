268478549eb65f9680c17723e1de43989099d940
SSSSSSL <dltmdtn116@naver.com>
2023-11-17 16:06:44 +0900

    update Jmeter_Mqtt_Influx

---
+++ JMeter_Mqtt/build.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+
+VERSION="1.0"
+TAG="jmeter-alpine:"$VERSION
+
+docker build -t $TAG . 
+
+
---
+++ JMeter_Mqtt/run.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+NAME=jmeter_mqtt_influx
+VERSION="1.0"                                                                                                                                            TAG="jmeter-alpine:"$VERSION 
+
+docker run -it --network=host --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY $TAG
