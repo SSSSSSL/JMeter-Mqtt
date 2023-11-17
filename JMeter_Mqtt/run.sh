103abb39104eaaec3fde5000be47cce7e0537c6c
SSSSSSL <dltmdtn116@naver.com>
2023-11-17 16:08:17 +0900

    add -d option

--- JMeter_Mqtt/run.sh
+++ JMeter_Mqtt/run.sh
@@ -3,4 +3,4 @@
 NAME=jmeter_mqtt_influx
 VERSION="1.0"                                                                                                                                            TAG="jmeter-alpine:"$VERSION 
 
-docker run -it --network=host --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY $TAG
+docker run -it -d --network=host --volume="$HOME/.Xauthority:/root/.Xauthority:rw" --volume=/tmp/.X11-unix:/tmp/.X11-unix:ro -e DISPLAY=$DISPLAY $TAG
